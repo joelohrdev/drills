c19351bedc0f456d1892c009014596c7f891f919
jltkd <jl@turnkeydigital.com>
2021-09-01 15:31:12 -0500

    Added Back Hands Drills

---
+++ pages/back-hands.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1 class="text-5xl font-bold mb-5">Back Hands</h1>
+    <ul role="list" class="divide-y divide-gray-200">
+      <li v-for="drill in drills" :key="drill.id" class="py-4 flex">
+        <div class="ml-3">
+          <a :href="[drill.link]" target="_blank" class="text-sm font-medium text-gray-900 hover:text-pink-500 transition">{{ drill.name }}</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const drills = [
+  {
+    id: 1,
+    name: 'Swivel Backhands',
+    link: 'https://www.instagram.com/p/BY3NBnNnOxx/?igshid=gpl199n205tl',
+  },
+  {
+    id: 2,
+    name: 'Rake Thru Backhands',
+    link: 'https://www.instagram.com/p/BTx382YhQm5/?igshid=4pw5vr4x86f5',
+  },
+  {
+    id: 3,
+    name: 'Advanced backhand short hops',
+    link: 'https://www.instagram.com/p/B6jqnjuA27q/?igshid=e2yxt5wj8x8a',
+  }
+]
+
+export default {
+  data() {
+    return {
+      drills,
+    }
+  },
+  setup() {
+    return {
+      drills,
+    }
+  },
+}
+</script>
