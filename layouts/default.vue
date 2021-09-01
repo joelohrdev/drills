3a122062c81da9171387eb471a5b41000feb4c9c
jltkd <jl@turnkeydigital.com>
2021-09-01 15:36:22 -0500

    Added Short Hops Drills

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="max-w-3xl mx-auto flex px-4 py-6 sm:px-6 lg:px-8">
-    <div class="w-full pt-12 lg:grid lg:grid-cols-4 lg:gap-x-8 xl:grid-cols-4">
+  <div class="max-w-4xl mx-auto flex px-4 py-6 sm:px-6 lg:px-8">
+    <div class="w-full pt-12 lg:grid lg:grid-cols-5 lg:gap-x-8 xl:grid-cols-5">
       <div class="mt-6 lg:mt-0 lg:col-span-2 xl:col-span-2">
         <Categories/>
       </div>
-      <div class="mt-6 lg:mt-0 lg:col-span-2 xl:col-span-2">
+      <div class="mt-6 lg:mt-0 lg:col-span-3 xl:col-span-3">
         <Nuxt/>
       </div>
     </div>
---
+++ pages/short-hops.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1 class="text-5xl font-bold mb-5">Short Hops</h1>
+    <ul role="list" class="divide-y divide-gray-200">
+      <li v-for="drill in drills" :key="drill.id" class="py-4 flex">
+        <div class="ml-3">
+          <a :href="[drill.link]" target="_blank" class="text-sm font-medium text-gray-900 hover:text-pink-500 transition">{{ drill.name }}</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const drills = [
+  {
+    id: 1,
+    name: 'Barehand Short Hop Progression w/ Stationary Feet',
+    link: 'https://www.instagram.com/p/B8keOLyAP_R/?igshid=1pmuvhfm5qcq6',
+  },
+  {
+    id: 2,
+    name: 'Riley Sartain Tweener Hops',
+    link: 'https://www.instagram.com/p/BwDk8EuBbYh/?igshid=1fspo141c10j9',
+  },
+  {
+    id: 3,
+    name: 'Quick Feet Tweener Hops Riley Sartain',
+    link: 'https://www.instagram.com/p/ByjNzYShOCM/?igshid=p65ucga1zoc6',
+  }
+]
+
+export default {
+  data() {
+    return {
+      drills,
+    }
+  },
+  setup() {
+    return {
+      drills,
+    }
+  },
+}
+</script>
